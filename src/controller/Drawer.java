af265b2c73d9ac5c1490b6e135292bdb0f6c84aa
Kevin <yorksgomez@gmail.com>
2019-02-15 11:52:23 -0500

    Added drawer.java

---
+++ src/controller/Drawer.java
@@ -0,0 +1,36 @@
+package controller;
+
+public class Drawer {
+
+    private int width, height;
+
+    public Drawer(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int widthToPercent(int width) {
+        return valToPercent(width, this.width);
+    }
+
+    public int heightToPercent(int height) {
+        return valToPercent(height, this.height);
+    }
+
+    public int valToPercent(int val, int max) {
+        return val * 100 / max;
+    }
+
+    public int widthToVal(int width) {
+        return percentToVal(width, this.width);
+    }
+
+    public int heightToVal(int height) {
+        return percentToVal(height, this.height);
+    }
+
+    public int percentToVal(int percent, int max) {
+        return percent * max / 100;
+    }
+
+}
