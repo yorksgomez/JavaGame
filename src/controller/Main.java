75e618d5a3f5533cd12949771752bfd03ba01d19
Kevin <yorksgomez@gmail.com>
2019-02-15 09:41:48 -0500

    Starting

---
+++ src/controller/Main.java
@@ -0,0 +1,33 @@
+package controller;
+
+import view.MainWindow;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Starting game...");
+
+        //Inserts look and feel
+        try {
+            System.out.println("Giving look and feel...");
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException ex) {
+            System.out.println("Error creating lookandfeel: " + ex.getMessage());
+        }
+
+        System.out.println("Look and feel gived correctly!");
+
+        //Creates main window
+        System.out.println("Starting main window...");
+        MainWindow mnWindow = new MainWindow();
+
+        SwingUtilities.invokeLater(() -> {
+            mnWindow.setVisible(true);
+            System.out.println("Window showed correctly!");
+        });
+
+    }
+
+}
